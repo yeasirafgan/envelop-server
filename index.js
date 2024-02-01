08a6acbe6607e30fcf8f1408a7aee2c91c90f876
Sher yeasir afgan <yeasir.one@gmail.com>
2024-02-01 10:17:28 +0000

    register route added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const userRoute = require("./routes/userRoute")
 
 // express app
 const app = express();
@@ -10,8 +11,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// endpoints
+app.use('/api/user', userRoute)
+
 // port
 const PORT = process.env.PORT || 4000;
+
 
 // connect to db
 mongoose.connect(process.env.MONGO_URL,{
